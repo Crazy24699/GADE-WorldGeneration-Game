cb535b7839737dba83d216389c3a7238129c862b
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-08-14 08:07:15 +0200

    Update

---
+++ WorldGeneration/Assets/Scripts/Player/DefenderHall.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DefenderHall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject TowerDefenderRef;
+    public float HalfDist;
+    [SerializeField] private List<GameObject> EnemySpawns = new List<GameObject>();
+
+    [SerializeField] private int TowerSpawnNum = 12;
+
+    public GameObject VertLocation;
+
+    [SerializeField] private float DefenderPlacementRange;
+
+    public Vector3 Scale;
+
+    public void DefenderStart()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            FindBarriers();
+            //Instantiate(Coob, new Vector3(0, 3, -15), Quaternion.identity).transform.localScale = Scale;
+            
+        }
+    }
+
+    private void SpawnDefenderTowers()
+    {
+
+    }
+    
+    private IEnumerator SpawnLoop()
+    {
+        yield return new WaitForSeconds(0.75f);
+        Instantiate(TowerDefenderRef);
+    }
+
+    private void FindBarriers()
+    {
+        GameObject GroundObject = GameObject.FindGameObjectWithTag("Ground");
+        MeshRenderer GroundMeshRendRef = GroundObject.GetComponent<MeshRenderer>();
+        MeshFilter GroundMeshFilterRef = GroundObject.GetComponent<MeshFilter>();
+
+        Mesh GroundMesh = GroundMeshFilterRef.sharedMesh;
+        foreach (var Vertex in GroundMesh.vertices)
+        {
+
+            //Debug.Log(Vertex);
+        }
+        Vector3 MaxPosition = GroundObject.transform.TransformPoint(GroundMesh.bounds.max);
+        Vector3 MinPosition = GroundObject.transform.TransformPoint(GroundMesh.bounds.min);
+
+        Instantiate(VertLocation, MaxPosition, Quaternion.identity);
+        Instantiate(VertLocation, MinPosition, Quaternion.identity);
+
+        HalfDist = MinPosition.z - MaxPosition.z;
+
+        Instantiate(VertLocation, new Vector3(0, 2, 1*HalfDist / 2.75f), Quaternion.identity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(new Vector3(0,3,-15), Scale);
+    }
+
+}
