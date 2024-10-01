b730472b2aa9e6cb9935e355c6ee68bbd9ebc357
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-10-01 13:51:30 +0200

    Saving last spawn position works
    The last spawn position saves and the enemy spawns there even after the max enemy limit has been reached

--- WorldGeneration/Assets/Scripts/World/EnemySpawnerLogic.cs
+++ WorldGeneration/Assets/Scripts/World/EnemySpawnerLogic.cs
@@ -11,12 +11,13 @@ public class EnemySpawnerLogic : MonoBehaviour
     [SerializeField] private GameObject[] Spawns;
     //[SerializeField] private List<bool> ReadyPaths;
     private HashSet<PathGenerator> MadePaths = new HashSet<PathGenerator>();
-    private GameObject LastSpawn;
+    [SerializeField]private GameObject LastSpawn;
 
     //remove this
     public bool Spawn = true;
 
-    private int CurrentSpawner = 0;
+    [SerializeField]private int AliveEnemies = 0;
+    public int CurrentSpawnNum;
 
     private void Start()
     {
@@ -31,19 +32,32 @@ public class EnemySpawnerLogic : MonoBehaviour
 
     private IEnumerator SpawnEnemies()
     {
-        yield return new WaitForSeconds(1);
+        Debug.Log("in a dream   ");
+        yield return new WaitForSeconds(0);
+
         for (int i = 0; i < 3; i++)
         {
-            if (ProgramManager.ProgramManagerInstance.EnemyCount.Count < 21)
+            if (AliveEnemies < 21)
             {
-                CreateEnemy("Normal", i);
-                Debug.Log("i nolonger dream, only nightmares of those whove died;");
+                yield return new WaitForSeconds(0.25f);
+                CreateEnemy("Normal", CurrentSpawnNum);
+                Debug.Log(CurrentSpawnNum);
+
+
+                Debug.Log("i nolonger dream, only nightmares of those whove died;       " + CurrentSpawnNum);
+                CurrentSpawnNum++;
             }
 
-
         }
+
+        if (CurrentSpawnNum > 2 && AliveEnemies <= 21)
+        {
+            CurrentSpawnNum = 0;
+        }
+
+
         yield return new WaitForSeconds(1);
-        
+
     }
 
     public void HandleSpawningState(bool SpawnEnemies)
@@ -69,9 +83,17 @@ public class EnemySpawnerLogic : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            AliveEnemies--;
+        }
+    }
+
     private void CreateEnemy(string EnemyType, int SpawnLocation)
     {
-        Debug.Log("monseter");
+        Debug.Log("monseter     "+SpawnLocation);
         GameObject EnemyCreated = null;
         GameObject ChosenEnemy = null;
         switch (EnemyType)
@@ -81,13 +103,15 @@ public class EnemySpawnerLogic : MonoBehaviour
                 ChosenEnemy = EnemyOptions[0].gameObject;
                 break;
         }
-        EnemyCreated = Instantiate(ChosenEnemy, Spawns[SpawnLocation].transform.position+new Vector3(2.5f,1.5f,0), Quaternion.identity);
+        EnemyCreated = Instantiate(ChosenEnemy, Spawns[SpawnLocation].transform.position + new Vector3(2.5f, 1.5f, 0), Quaternion.identity);
         EnemyCreated.GetComponent<BaseEnemy>().Startup();
         int Index = Spawns[SpawnLocation].GetComponent<PathGenerator>().ControlPointTransforms.Count();
 
-        EnemyCreated.GetComponent<BasicEnemy>().FinalTarget = Spawns[SpawnLocation].GetComponent<PathGenerator>().ControlPointTransforms[Index-1].gameObject;
-        EnemyCreated.gameObject.name = "Normal Enemy"+ProgramManager.ProgramManagerInstance.EnemyCount.Count;
+        EnemyCreated.GetComponent<BasicEnemy>().FinalTarget = Spawns[SpawnLocation].GetComponent<PathGenerator>().ControlPointTransforms[Index - 1].gameObject;
+        EnemyCreated.gameObject.name = "Normal Enemy" + ProgramManager.ProgramManagerInstance.EnemyCount.Count;
         ProgramManager.ProgramManagerInstance.EnemyCount.Add(EnemyCreated);
+        AliveEnemies++;
+        LastSpawn = Spawns[SpawnLocation];
     }
 
 }
